b951fc03dd157318926ddd5f577b1f1495e58213
早川修平 <hykwshuhei@hayakawashuuheinoMacBook-Air.local>
2023-05-17 13:46:28 +0900

    second commit

--- pages/articleList.vue
+++ pages/articleList.vue
@@ -3,158 +3,238 @@
     <div class="flex">
       <!-- カテゴリ検索欄 -->
       <!-- プログラミング言語 -->
-      <div class="py-24 mr-5">
-        <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
-          プログラミング言語
-        </h3>
-        <ul
-          class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        >
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+      <div class="flex flex-col">
+        <div class="pt-24 mr-5">
+          <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
+            プログラミング言語
+          </h3>
+          <ul
+            class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           >
-            <div class="flex items-center pl-3">
-              <input
-                id="vue-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="vue-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Vue JS</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="vue-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="vue-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >Vue JS</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="react-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="react-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >React</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="angular-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="angular-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >Angular</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="laravel-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="laravel-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >Laravel</label
+                >
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- 職種 -->
+        <div class="pt-12 mr-5">
+          <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">職種</h3>
+          <ul
+            class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           >
-            <div class="flex items-center pl-3">
-              <input
-                id="react-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="react-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >React</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="vue-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="vue-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >FR</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="react-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="react-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >ML</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="angular-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="angular-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >Java</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="laravel-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="laravel-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >PHP</label
+                >
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- サークル -->
+        <div class="pt-12 mr-5">
+          <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
+            サークル
+          </h3>
+          <ul
+            class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
           >
-            <div class="flex items-center pl-3">
-              <input
-                id="angular-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="angular-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Angular</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
-          >
-            <div class="flex items-center pl-3">
-              <input
-                id="laravel-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="laravel-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Laravel</label
-              >
-            </div>
-          </li>
-        </ul>
-      </div>
-      <!-- 職種 -->
-      <div class="py-24 mr-5">
-        <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">職種</h3>
-        <ul
-          class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-        >
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
-          >
-            <div class="flex items-center pl-3">
-              <input
-                id="vue-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="vue-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >FR</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
-          >
-            <div class="flex items-center pl-3">
-              <input
-                id="react-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="react-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >ML</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
-          >
-            <div class="flex items-center pl-3">
-              <input
-                id="angular-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="angular-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Java</label
-              >
-            </div>
-          </li>
-          <li
-            class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
-          >
-            <div class="flex items-center pl-3">
-              <input
-                id="laravel-checkbox"
-                type="checkbox"
-                value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
-              <label
-                for="laravel-checkbox"
-                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >PHP</label
-              >
-            </div>
-          </li>
-        </ul>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="vue-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="vue-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >サークル名</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="react-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="react-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >サークル名</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="angular-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="angular-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >サークル名</label
+                >
+              </div>
+            </li>
+            <li
+              class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+            >
+              <div class="flex items-center pl-3">
+                <input
+                  id="laravel-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                />
+                <label
+                  for="laravel-checkbox"
+                  class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >サークル名</label
+                >
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <!-- <div class="w-1/4 p-4">
