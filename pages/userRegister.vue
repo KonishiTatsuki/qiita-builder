ad5d355d4185721ef0bd4ad2f4d442d1832b74d6
Madoka Kato <maka7816@yahoo.co.jp>
2023-05-17 13:46:42 +0900

    新規登録ページのhtml作成

---
+++ pages/userRegister.vue
@@ -0,0 +1,32 @@
+<template>
+  <form action="">
+    <div>
+      <p>新規登録</p>
+      <div>
+        メールアドレス
+        <div><input type="text" class="rounded-xl border-black" /></div>
+      </div>
+      <div>
+        パスワード<dvi><input type="text" /></dvi>
+      </div>
+      <div>
+        パスワード確認用
+        <div><input type="text" /></div>
+      </div>
+    </div>
+    c
+    <div>
+      サークル名
+      <select name="" id="">
+        <option value=""></option>
+        <input type="text" />
+      </select>
+    </div>
+    <div>
+      職種
+      <button class="rounded-xl border-black">JAVA</button>
+    </div>
+    <div>アイコン画像</div>
+    <div>自己紹介<textarea name="" id="" cols="30" rows="10"></textarea></div>
+  </form>
+</template>
