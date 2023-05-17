8941ad6eded0ef9d5bbb56ff02a10410763956f3
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-05-17 15:33:55 +0900

    ログイン画面作成

--- pages/login.vue
+++ pages/login.vue
@@ -23,36 +23,38 @@
       <div class="flex justify-center mt-20">
         <div class="w-9/12 border">
           <div class="my-16 text-center">
-            <h2 class="text-4xl font-bold">ログイン</h2>
+            <h1 class="title font-bold">ログイン</h1>
             <form action="/form/login" method="POST" class="mt-12">
               <div></div>
               <div class="mb-3">
-                <label class="text-xs pr-5">メールアドレス</label>
+                <label class=" pr-5">メールアドレス</label>
                 <input
                   type="email"
                   placeholder="you@rakus-partners.co.jp"
                   name="email"
-                  class="text-xs w-4/12 border rounded"
+                  maxlength='30'
+                  class=" w-4/12 border rounded "
                 />
               </div>
               <div class="mb-5">
-                <label class="text-xs pr-5">パスワード</label>
+                <label class=" pr-5">パスワード</label>
                 <input
                   type="password"
                   placeholder="パスワード"
                   name="password"
-                  class="text-xs w-4/12 border rounded"
+                  maxlength='30'
+                  class=" w-4/12 border rounded"
                 />
               </div>
               <button
                 type="submit"
-                class="mb-3 text-xs w-4/10 py-2 border rounded hover:opacity-75"
+                class="mb-3  w-4/10 py-2 border rounded hover:opacity-75"
               >
                 新規登録
               </button>
               <button
                 type="submit"
-                class="mb-3 text-xs w-4/10 bg-blue-800 text-white py-2 rounded hover:opacity-75"
+                class="mb-3  w-4/10 bg-blue-800 text-white py-2 rounded hover:opacity-75"
               >
                 ログイン
               </button>
