b77bd44e72c8920fe48840f35b00287c5be8c0d8
Madoka Kato <maka7816@yahoo.co.jp>
2023-05-17 14:56:45 +0900

    新規登録画面のhtml作成完了

--- pages/userRegister.vue
+++ pages/userRegister.vue
@@ -1,32 +1,78 @@
 <template>
-  <form action="">
-    <div>
-      <p>新規登録</p>
+  <div>
+    <form action="">
       <div>
-        メールアドレス
-        <div><input type="text" class="rounded-xl border-black" /></div>
+        <p class="text-4xl mb-2">新規登録</p>
+        <div class="mb-2">
+          メールアドレス
+          <div><input type="text" class="border border-black" /></div>
+        </div>
+        <div class="mb-2">
+          パスワード
+          <div><input type="text" class="border border-black" /></div>
+        </div>
+        <div class="mb-2">
+          パスワード確認用
+          <div><input type="text" class="border border-black" /></div>
+        </div>
+      </div>
+      <div class="mb-2">
+        サークル名
+        <div>
+          <select name="" id="">
+            <option value="">その他</option>
+            <input type="text" class="border border-black" />
+          </select>
+        </div>
+      </div>
+      <div class="mb-2">
+        職種
+        <div class="flex">
+          <button type="button" class="rounded-xl border border-black w-11">
+            JAVA
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            CL
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            PHP
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            FR
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            ML
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            QA
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            営業
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            内勤
+          </button>
+          <button type="button" class="rounded-xl border border-black w-11">
+            CS
+          </button>
+        </div>
+      </div>
+      <div class="mb-2">
+        アイコン画像
+        <div><button class="border-black border">ファイルを保存</button></div>
       </div>
       <div>
-        パスワード<dvi><input type="text" /></dvi>
+        自己紹介
+        <div>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+            class="border border-black"
+          ></textarea>
+        </div>
       </div>
-      <div>
-        パスワード確認用
-        <div><input type="text" /></div>
-      </div>
-    </div>
-    c
-    <div>
-      サークル名
-      <select name="" id="">
-        <option value=""></option>
-        <input type="text" />
-      </select>
-    </div>
-    <div>
-      職種
-      <button class="rounded-xl border-black">JAVA</button>
-    </div>
-    <div>アイコン画像</div>
-    <div>自己紹介<textarea name="" id="" cols="30" rows="10"></textarea></div>
-  </form>
+    </form>
+  </div>
 </template>
