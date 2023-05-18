94e3f74bbaf087aa4f8daab4529629b4072a9b39
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2023-05-18 09:50:44 +0900

    共通レイアウトh2タブ アドベントカレンダー新規作成画面

---
+++ pages/createAdvent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="title">アドベントカレンダー新規作成</h1>
+    <h2 class="subtitle">アドベントカレンダーの題名</h2>
+    <input
+      maxlength="30"
+      type="text"
+      name="title"
+      class="border border-blue-500 w-96"
+    />
+    <h2 class="subtitle">アドベントカレンダーの説明</h2>
+    <textarea
+      name="body"
+      cols="100"
+      rows="7"
+      maxlength="255"
+      class="border border-blue-500"
+    ></textarea>
+    <h2 class="subtitle">期間</h2>
+    <h2 class="subtitle">バナー画像</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
