f6158ee2d582a91a3b85c1a2eedf9da40ed1de2c
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-05-23 16:37:01 +0900

    :+1: パスワード変更メール送信機能作成

--- pages/passwordForget.vue
+++ pages/passwordForget.vue
@@ -5,28 +5,30 @@
         <div class="text-center">
           <h1 class="title">パスワードを忘れた場合</h1>
           <div class="flex justify-center">
-            <form>
-              <div class="mb-5 text-left">
-                メールアドレス
-                <div>
-                  <input
-                    type="email"
-                    maxlength="255"
-                    placeholder="you@rakus-partners.co.jp"
-                    class="border rounded border-black"
-                    v-model="email"
-                  />
-                </div>
+            <FormKit
+              type="form"
+              @submit="submit"
+              submit-label="パスワードを忘れた場合"
+            >
+              <div class="mb-5 text-center">
+                <FormKit
+                  :classes="{
+                    input: 'border border-black py-1 px-2 rounded-md',
+                  }"
+                  type="email"
+                  label=" メールアドレス"
+                  name="email"
+                  validation="required|matches:/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/|ends_with:rakus-partners.co.jp"
+                  autocomplete="off"
+                  :validation-messages="{
+                    required: 'メールアドレスを入力してください',
+                    matches: '正しいメールアドレスを入力してください',
+                    ends_with: 'ラクスのメールアドレスを入力してください',
+                  }"
+                />
               </div>
-            </form>
+            </FormKit>
           </div>
-          <button
-            type="submit"
-            class="btn mb-5"
-            @click="signIn({ email: email })"
-          >
-            パスワード再設定用メール送付
-          </button>
           <p>上記メールアドレスに再設定用のURLを送付します。</p>
         </div>
       </div>
@@ -35,25 +37,22 @@
 </template>
 
 <script setup>
+const router = useRouter();
 const supabase = useSupabaseClient();
 
 const email = ref("");
-const password = ref("");
-const loading = ref(false);
 
-const signIn = async function (e) {
-  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `http://localhost:3000/passwordReset`,
-  });
-  if (error) {
-    throw error;
-  }
-  console.log("メールを送信しました。");
-  email.value = "";
-  password.value = "";
+const submit = async (submit) => {
+  console.log(submit.email);
+  await supabase.auth.resetPasswordForEmail(
+    "tatsuki.konishi@rakus-partners.co.jp",
+    {
+      redirectTo: "http://localhost:3000/passwordReset",
+    }
+  );
 };
 </script>
-W
+
 <style scoped>
 .main {
   min-height: calc(100vh - 168px);
