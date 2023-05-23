be8726c0b1929fd91cc5732fa0ec785368abec4e
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-05-23 11:00:52 +0900

    :+1: ログイン機能作成

--- pages/login.vue
+++ pages/login.vue
@@ -1,71 +1,92 @@
 <template>
-  <div class="flex main">
+  <!-- <div class="flex main">
     <div class="flex-auto my-auto">
       <div class="flex justify-center">
         <div class="text-center">
           <h1 class="title">ログイン</h1>
           <div class="flex justify-center">
-            <form>
+            <FormKit
+              type="form"
+              @submit="signInWithEmail"
+              submit-label="ログイン"
+            >
               <div class="mb-2 text-left">
-                メールアドレス
-                <div>
-                  <input
-                    type="email"
-                    maxlength="255"
-                    placeholder="you@rakus-partners.co.jp"
-                    class="border rounded border-black"
-                    v-model="email"
-                  />
-                </div>
+                <FormKit
+                  :classes="{
+                    input: 'border border-black py-1 px-2 rounded-md',
+                  }"
+                  type="email"
+                  label=" メールアドレス"
+                  name="email"
+                  validation="required|matches:/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/|ends_with:rakus-partners.co.jp"
+                  autocomplete="off"
+                  :validation-messages="{
+                    required: 'メールアドレスを入力してください',
+                    matches: '正しいメールアドレスを入力してください',
+                    ends_with: 'ラクスのメールアドレスを入力してください',
+                  }"
+                />
               </div>
               <div class="mb-5 text-left">
-                パスワード
-                <div>
-                  <input
-                    type="password"
-                    maxlength="30"
-                    placeholder="password"
-                    class="border rounded border-black"
-                    v-model="password"
-                  />
-                </div>
+                <FormKit
+                  :classes="{
+                    input: 'border border-black py-1 px-2 rounded-md',
+                  }"
+                  type="password"
+                  label=" パスワード"
+                  name="password"
+                  validation="required|length:8,30|contains_numeric|contains_lowercase|contains_uppercase"
+                  autocomplete="off"
+                  :validation-messages="{
+                    required: 'パスワードを入力してください',
+                    length: '8文字以上30文字以内で入力してください',
+                    contains_numeric:
+                      '半角英小文字・大文字・数字をそれぞれ1種類以上含んでください',
+                    contains_lowercase:
+                      '半角英小文字・大文字・数字をそれぞれ1種類以上含んでください',
+                    contains_uppercase:
+                      '半角英小文字・大文字・数字をそれぞれ1種類以上含んでください',
+                  }"
+                />
               </div>
-            </form>
+              <button
+                type="submit"
+                class="px-5 py-2 rounded-md text-base border hover:bg-[#1D8EB9] hover:text-white hover:border-indigo-700 mr-5"
+              >
+                <NuxtLink to="/userRegister">新規登録</NuxtLink>
+              </button>
+            </FormKit>
           </div>
-          <button
-            type="submit"
-            class="px-5 py-2 rounded-md text-base border hover:bg-[#1D8EB9] hover:text-white hover:border-indigo-700 mr-5"
-          >
-            <NuxtLink to="/userRegister">新規登録</NuxtLink>
-          </button>
-          <button
-            type="button"
-            class="btn"
-            @click="signInWithEmail({ email: email, password: password })"
-          >
-            ログイン
-          </button>
         </div>
       </div>
     </div>
+  </div> -->
+  <div class="container">
+    <form @submit.prevent="submit">
+      <input type="email" v-model="email" />
+      <input type="password" v-model="password" />
+      <input type="submit" value="ログイン" />
+    </form>
   </div>
 </template>
 
 <script setup>
+const router = useRouter();
+const client = useSupabaseClient();
+
 const email = ref("");
 const password = ref("");
 
-async function signInWithEmail(e) {
-  const { data, error } = await supabase.auth.signInWithPassword({
+const submit = async () => {
+  console.log(email.value, password.value);
+  const session = await client.auth.signIn({
     email: email.value,
     password: password.value,
   });
-  if (error) {
-    throw error;
+  if (session) {
+    location.href = "/";
   }
-  email.value = "";
-  password.value = "";
-}
+};
 </script>
 
 <style scoped>
