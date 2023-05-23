1d47907ac2eab0f211687874a56d6bce4dde64ac
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2023-05-23 15:48:06 +0900

    :sparkles: 記事投稿と編集機能の実装(ユーザー情報の識別なし)

--- pages/articles/edit/[id].vue
+++ pages/articles/edit/[id].vue
@@ -38,7 +38,7 @@
         </div>
       <div>
         <span class="mr-4">
-            <button type="submit" class="btn text-right" @click="handleSubmit">投稿する</button>
+            <button type="submit" class="btn text-right" @click="handleSubmit">編集を完了する</button>
         </span>
         <span>
             <button type="submit" class="border py-2 px-2 rounded-md" @click="draftSubmit">下書き保存</button>
@@ -49,16 +49,22 @@
 </template>
 
 <script lang="ts" setup>
+const route = useRoute();
+// パスパラメータよりid取得
+const { id } = route.params;
+const { data: article } = await useFetch(`http://localhost:8000/article/${id}`)
+
+console.log("value", (article.value.id))
 import type EasyMDE from "easymde";
-import { ref, onMounted } from "vue"
+import { ref, onMounted}  from "vue"
 
 let mde: InstanceType<typeof EasyMDE> | null = null;
 
-const content = ref('');
+const content = ref(article.value.body);
 const contentArea = ref();
-const title = ref('')
-const goalLike = ref('')
-const publishDate = ref('')
+const title = ref(article.value.title)
+const goalLike = ref(article.value.goalLike)
+const publishDate = ref(article.value.publishDate)
 const router = useRouter()
 
 const handleSubmit= async () => {
@@ -76,8 +82,8 @@ const handleSubmit= async () => {
         publishDate: publishDate,
         publish: true,
     }
-  await useFetch('http://localhost:8000/article', {
-  method: 'POST',
+  await useFetch(`http://localhost:8000/article/${id}`, {
+  method: 'PATCH',
   body: postData,
 });
     router.push('/')
@@ -97,8 +103,8 @@ const draftSubmit= async () => {
         publishDate: publishDate,
         publish: false
     }
-  await useFetch('http://localhost:8000/article', {
-  method: 'POST',
+  await useFetch(`http://localhost:8000/article/${id}`, {
+  method: 'PATCH',
   body: postData,
 })
     router.push('/')
