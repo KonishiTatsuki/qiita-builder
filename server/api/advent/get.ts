77053650b17de716e9917a1016bbea46d5921a05
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2023-05-26 23:11:04 +0900

    :+1: カレンダーの作成ロジックの変更

--- server/api/advent/get.ts
+++ server/api/advent/get.ts
@@ -1,8 +1,7 @@
 import { serverSupabaseClient } from "#supabase/server";
-
 export default defineEventHandler(async (event) => {
-  const user = useSupabaseUser();
   const client = serverSupabaseClient(event);
-  const { data } = await client.from("article").select("id, userId, title")
+  const { data } = await client.from("banner").select("*,userId(username)").eq("id", 5);
+
   return data;
-});
+});;
