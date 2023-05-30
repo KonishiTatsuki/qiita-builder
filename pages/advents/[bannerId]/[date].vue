88fb14244ea12cc6ce49d25b18efad75e3b8285b
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2023-05-30 16:07:28 +0900

    datepickerがpushできるかのテスト

--- pages/advents/[bannerId]/[date].vue
+++ pages/advents/[bannerId]/[date].vue
@@ -9,14 +9,28 @@
       <p>入力したタイトル: {{ title }}</p>
     </div>
     <h2 class="subtitle">記事の選択</h2>
-    <div v-for="(article, index) in articles" :key="article.id">
+    <div v-for="article in articles" :key="article.id">
       <div>
-        {{ article.title }}- index:{{ index }}-publishDate:
-        {{ article.publishDate }}
+        <label
+          for="countries_multiple"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Select an option</label
+        >
+        <select
+          multiple
+          id="countries_multiple"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Choose countries</option>
+          <option value="US">United States</option>
+          <option value="CA">Canada</option>
+          <option value="FR">France</option>
+          <option value="DE">Germany</option>
+        </select>
       </div>
     </div>
     <div class="mb-2">
-      <select name="" id="">
+      <select name="" id="" class="p-200">
         <option value="">記事１</option>
       </select>
     </div>
