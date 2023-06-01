d007d0d3f3d38e7bb929d3287b593a2312eb1b67
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-06-01 11:27:16 +0900

    :+1: TS修正中

--- server/api/user/qiitaTokenUpdate.ts
+++ server/api/user/qiitaTokenUpdate.ts
@@ -2,7 +2,7 @@ import { serverSupabaseClient } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-  console.log("body", body);
+  console.log(body);
   const client = serverSupabaseClient(event);
   const { data } = await client
     .from("profiles")
@@ -10,3 +10,19 @@ export default defineEventHandler(async (event) => {
     .eq("id", body.userId);
   return data;
 });
+
+// import { serverSupabaseClient } from "#supabase/server";
+// interface BodySchema {
+//   userId: string;
+//   qiitaToken: string;
+// }
+// export default defineEventHandler(async (event) => {
+//   const body = await readBody<BodySchema>(event);
+//   console.log("body", body);
+//   const client = serverSupabaseClient(event);
+//   const { data } = await client
+//     .from("profiles")
+//     .update({ qiitaToken: body.qiitaToken })
+//     .eq("id", body.userId);
+//   return data;
+// });
