1d92fe67dfbbed5f8bab231fcdd712e36ce27714
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2023-06-08 15:27:45 +0900

    :+1: ログインしたアカウントと記事投稿したuserNameが同じときだけ編集ボタン

--- pages/calendar/[id].vue
+++ pages/calendar/[id].vue
@@ -83,6 +83,7 @@
                   <NuxtLink :to="`/advents/edit/${id}/${day.date}`">
                     <button
                       v-if="
+                        article.userId.username.slice(0, 10) === userName &&
                         day.isCurrentMonth &&
                         day.period &&
                         matchingArticles(day.date).length === 1 &&
@@ -107,7 +108,6 @@
 import { ref, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import dayjs from "dayjs";
-
 useHead({
   title: "アドベントカレンダー",
 });
@@ -119,6 +119,9 @@ const startDate = ref("");
 const endDate = ref("");
 const managerName = ref("");
 const route = useRoute();
+const user = useSupabaseUser();
+const userName = ref(user.value.user_metadata.username);
+console.log("user", user.value.user_metadata.username);
 
 // bannerテーブル情報を取得
 const { id } = route.params;
