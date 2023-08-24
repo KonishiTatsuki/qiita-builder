447fab7a3f272cf338ec3bdb6d01a4a9d5b07712
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2023-08-24 16:52:32 +0900

    :sparkles: アクセス権限がないユーザーのリダイレクト

--- pages/managerAuthorityError.vue
+++ pages/managerAuthorityError.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="text-center main">
-    <div class="text-2xl">このページにはアクセスできません</div>
-    <hr class="my-12" />
-    <div>管理者権限を持っていないユーザーです</div>
-    <div>5秒後にマイページに遷移します</div>
+    <div class="text-2xl pt-24 tracking-widest">
+      <span class="mdi mdi-block-helper text-red-500"></span>
+      このページにはアクセスできません
+    </div>
+    <hr class="my-9" />
+    <div>管理者権限がないユーザーです</div>
+    <div class="pt-3">5秒後にマイページに遷移します</div>
   </div>
 </template>
 
-<script></script>
+<script>
+setTimeout(function () {
+  window.location = "http://localhost:3000/mypage";
+}, 5000);
+</script>
 
 <style></style>
