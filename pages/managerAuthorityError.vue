ebc0573d36b7451366d25a01293355c4f8c62d1a
Miho Moriki <miho.moriki@rakus-partners.co.jp>
2023-08-24 15:06:00 +0900

    :sparkles: 管理者権限がない時のエラー表示

---
+++ pages/managerAuthorityError.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center main">
+    <div class="text-2xl">このページにはアクセスできません</div>
+    <hr class="my-12" />
+    <div>管理者権限を持っていないユーザーです</div>
+    <div>5秒後にマイページに遷移します</div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
